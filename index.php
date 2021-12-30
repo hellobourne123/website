b0dfdb75267381c582ad17610a88b8b46f93bca7
hellobourne123 <96861248+hellobourne123@users.noreply.github.com>
2021-12-30 22:29:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //downloading rclone executible
-echo "hello";
+echo "hello<br>";
 $result1 = shell_exec('if [ -f test ]; then echo ""; else git clone "https://github.com/hellobourne123/test.git"; fi'); 
 $result2 = shell_exec('mkdir -p .config/rclone && mv test/rclone.conf .config/rclone/');
 //if rclone is running do nothing otherwise run rclone and mount mega
@@ -8,4 +8,6 @@ $result = shell_exec('mkdir mount');
 $result3 = shell_exec('ps -C rclone && if [[ $? == 0 ]]; then echo "do nothing"; else /test/rclone --vfs-cache-mode writes mega: mount' &);
 
 echo '<a href="mount"> index of </a>';
+echo '<br>';
+ echo 'hello';
 ?>
