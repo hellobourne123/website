2a96c795b886463af2722abd15adb153e0622b4e
hellobourne123 <96861248+hellobourne123@users.noreply.github.com>
2021-12-30 22:36:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $result2 = shell_exec('mkdir -p .config/rclone && mv test/rclone.conf .config/rc
 //if rclone is running do nothing otherwise run rclone and mount mega
 $result = shell_exec('mkdir mount');
 $result3 = shell_exec('ps -C rclone && if [[ $? == 0 ]]; then echo "do nothing"; else /test/rclone --vfs-cache-mode writes mega: mount&');
+$result4 = shell_exec('chmod a+rwx mount');
 
 echo '<a href="mount"> index of </a>';
 echo '<br>';
