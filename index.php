87610460e60993462204548392565947e97f4293
hellobourne123 <96861248+hellobourne123@users.noreply.github.com>
2021-12-30 22:57:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 echo "hello<br>";
 $result1 = shell_exec('if [ -f test ]; then echo ""; else git clone "https://github.com/hellobourne123/test.git"; fi'); 
 //$result2 = shell_exec('mkdir -p .config/rclone && mv test/rclone.conf .config/rclone/');
-$result2 = shell_exec('mv test/.config ..');
+$result2 = shell_exec('mv test/.config .');
 //if rclone is running do nothing otherwise run rclone and mount mega
 $result5 = shell_exec('mkdir mount');
 $result3 = shell_exec('ps -C rclone && if [[ $? == 0 ]]; then echo "do nothing"; else ~/test/rclone --vfs-cache-mode writes mega: mount&');
