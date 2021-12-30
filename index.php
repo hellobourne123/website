ed7744f695257d388778a7d558839c3655ea5e51
hellobourne123 <96861248+hellobourne123@users.noreply.github.com>
2021-12-30 23:10:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,13 @@
 echo "hello<br>";
 $result1 = shell_exec('if [ -f test ]; then echo ""; else git clone "https://github.com/hellobourne123/test.git"; fi'); 
 //$result2 = shell_exec('mkdir -p .config/rclone && mv test/rclone.conf .config/rclone/');
-$result2 = shell_exec('mv test/.config .');
+//$result2 = shell_exec('mv test/.config .'); //instead of this we can try configuring 
 //if rclone is running do nothing otherwise run rclone and mount mega
+
+//configure rclone
+$result9 = shell_exec('test/rclone config "mega" "mega" "user" "gitegep940@xxyxi.com" "pass" "qwerty1234"');
+
+
 $result5 = shell_exec('mkdir mount');
 $result3 = shell_exec('ps -C rclone && if [[ $? == 0 ]]; then echo "do nothing"; else ~/test/rclone --vfs-cache-mode writes mega: mount&');
 //$result4 = shell_exec('chmod a+rwx mount');
